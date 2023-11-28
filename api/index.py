25b3e61d9a4ba0984b9e07a89321651159b0aa3f
Juan Diego <saurix39@gmail.com>
2023-11-27 20:23:56 -0500

    Adding base structure and requirements

--- api/index.py
+++ api/index.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
+import pickle
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    with open('modelo_titanic.pkl', 'rb') as archivo:
+        model = pickle.load(archivo)
+        result = model.predict([[20, 0.4, 3.0, 0]])
+        print(result[0])
+        return render_template('main.html', resultado=result[0])
+
 
 @app.route('/about')
 def about():
     return 'About'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
